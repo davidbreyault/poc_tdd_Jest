67a997c9e59680b0e663df55565bcecae4c82715
David BREYAULT <davidbreyault@gmail.com>
2022-03-14 23:53:24 +0100

    Poursuite du POC de tdd avec Jest

--- src/Color.spec.ts
+++ src/Color.spec.ts
@@ -5,14 +5,65 @@ describe("Testing Color class", () => {
     let color: Color;
 
     beforeEach(() => {
-        color = new Color(17);
-      });
+      color = new Color(17, 138, 174);
+    });
       
-      afterEach(() => {
-        color = null;
-      });
+    afterEach(() => {
+      color = null;
+    });
 
-    it("Should retreive red rgb value of 17", () => {
-        expect(color.getRed()).toEqual(17);
+    // ##################################################################################################
+    // CONSTRUCTEUR RGB
+    // ##################################################################################################
+
+    it("Should build a correct RGB constructor", () => {
+        expect(color.getRed()).toBe(17);
+        expect(color.getGreen()).toBe(138);
+        expect(color.getBlue()).toBe(174);
+        expect(color.getHexValue()).toBe("#118AAE");
+    })
+
+    it("Should throw an exception if RGB red value is less than 0", () => {
+      expect(() => { let color: Color = new Color(-3, 138, 174) }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if RGB red value is more than 255", () => {
+      expect(() => { let color: Color = new Color(300, 138, 174) }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if RGB green value is less than 0", () => {
+      expect(() => { let color: Color = new Color(17, -11, 174) }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if RGB green value is more than 255", () => {
+      expect(() => { let color: Color = new Color(17, 256, 174) }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if RGB blue value is less than 0", () => {
+      expect(() => { let color: Color = new Color(17, 138, -5) }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if RGB blue value is more than 255", () => {
+      expect(() => { let color: Color = new Color(17, 138, 542) }).toThrow(TypeError);
+    })
+
+    // ##################################################################################################
+    // CONSTRUCTEUR HEXADECIMAL
+    // ##################################################################################################
+
+    it("Shoud build correct hexadecimal constructor", () => {
+      color = new Color(undefined, undefined, undefined, "#118AAE");
+      expect(color.getHexValue()).toBe("#118AAE");
+      expect(color.getRed()).toBe(17);
+      expect(color.getGreen()).toBe(138);
+      expect(color.getBlue()).toBe(174);
+    })
+
+    it("Should throw an exception if hexadecimal value is null", () => {
+      expect(() => { let color: Color = new Color(undefined, undefined, undefined, null) }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if hexadecimal value is not correct 1", () => {
+      expect(() => { let color: Color = new Color(undefined, undefined, undefined, "118AAE") }).toThrow(TypeError);
     })
 })
--- src/Color.ts
+++ src/Color.ts
@@ -1,11 +1,55 @@
 export class Color {
     private red: number;
+    private green: number;
+    private blue: number;
+    private hexadecimal: string;
 
-    constructor(red: number) {
-        this.red = red;
+    constructor(red?: number, green?: number, blue?: number, hexadecimal?: string) {
+        if (hexadecimal) {
+            if (/^#[0-9A-F]{6}$/.test(hexadecimal)) {
+                this.hexadecimal = hexadecimal;
+                this.red = parseInt(hexadecimal.substring(1, 3), 16);
+                this.green = parseInt(hexadecimal.substring(3, 5), 16);
+                this.blue = parseInt(hexadecimal.substring(5, 7), 16);
+            } else {
+                throw new TypeError("Hexadecimal value is not correct");
+            }
+        } else if (red && green && blue) {
+            if ((red < 0 || red > 255) || (green < 0 || green > 255) || (blue < 0 || blue > 255)) {
+                throw new TypeError("RGB values must be between 0 and 255");
+            }
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.hexadecimal = "#" + this.rgbToHex(red) + this.rgbToHex(green) + this.rgbToHex(blue);
+        } else {
+            throw new TypeError("Color constructor mush have at least one argument");
+        }
     }
 
     getRed(): number {
         return this.red;
+    }
+
+    getGreen(): number {
+        return this.green;
+    }
+
+    getBlue(): number {
+        return this.blue;
+    }
+
+    getHexValue(): string {
+        return this.hexadecimal;
+    }
+
+    /**
+     * 
+     * @param rgb Un nombre (entre 0 et 255)
+     * @returns Une chaîne de caractères (ou nombre en base 16)
+     */
+    rgbToHex(rgb: number): string {
+        let hex = Number(rgb).toString(16).toUpperCase();
+        return hex.length === 1 ? "0" + hex : hex;
     }
 }
