f672f59b6f95246b3d873098ac31884730e8292f
David BREYAULT <davidbreyault@gmail.com>
2022-03-15 23:40:58 +0100

    Poursuite et fin du POC de tdd avec Jest

--- src/Color.spec.ts
+++ src/Color.spec.ts
@@ -51,8 +51,8 @@ describe("Testing Color class", () => {
     // CONSTRUCTEUR HEXADECIMAL
     // ##################################################################################################
 
-    it("Shoud build correct hexadecimal constructor", () => {
-      color = new Color(undefined, undefined, undefined, "#118AAE");
+    it("Should build correct hexadecimal constructor", () => {
+      color = new Color("#118AAE");
       expect(color.getHexValue()).toBe("#118AAE");
       expect(color.getRed()).toBe(17);
       expect(color.getGreen()).toBe(138);
@@ -60,10 +60,158 @@ describe("Testing Color class", () => {
     })
 
     it("Should throw an exception if hexadecimal value is null", () => {
-      expect(() => { let color: Color = new Color(undefined, undefined, undefined, null) }).toThrow(TypeError);
+      expect(() => { let color: Color = new Color(null) }).toThrow(TypeError);
     })
 
     it("Should throw an exception if hexadecimal value is not correct 1", () => {
-      expect(() => { let color: Color = new Color(undefined, undefined, undefined, "118AAE") }).toThrow(TypeError);
+      expect(() => { let color: Color = new Color("118AAE") }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if hexadecimal value is not correct 2", () => {
+      expect(() => { let color: Color = new Color("#118AA") }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if hexadecimal value is not correct 3", () => {
+      expect(() => { let color: Color = new Color("#118AAE1") }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if hexadecimal value is not correct 4", () => {
+      expect(() => { let color: Color = new Color("#118AAG") }).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if hexadecimal value is not correct 5", () => {
+      expect(() => { let color: Color = new Color("#118aae") }).toThrow(TypeError);
+    })
+
+    // ##################################################################################################
+    // GETTERS
+    // ##################################################################################################
+
+    it("Testing getRed method", () => {
+      expect(color.getRed()).toBe(17);
+      expect(color.getGreen()).toBe(138);
+      expect(color.getBlue()).toBe(174);
+      expect(color.getHexValue()).toBe("#118AAE");
+    })
+
+    it("Testing getGreen method", () => {
+      expect(color.getGreen()).toBe(138);
+      expect(color.getRed()).toBe(17);
+      expect(color.getBlue()).toBe(174);
+      expect(color.getHexValue()).toBe("#118AAE");
+    })
+
+    it("Testing getBlue method", () => {
+      expect(color.getBlue()).toBe(174);
+      expect(color.getRed()).toBe(17);
+      expect(color.getGreen()).toBe(138);
+      expect(color.getHexValue()).toBe("#118AAE");
+    })
+
+    it("Testing getHexValue method", () => {
+      expect(color.getHexValue()).toBe("#118AAE");
+      expect(color.getRed()).toBe(17);
+      expect(color.getGreen()).toBe(138);
+      expect(color.getBlue()).toBe(174);
+    })
+
+    // ##################################################################################################
+    // SETTER
+    // ##################################################################################################
+
+    // RED
+    it("Testing setRed method", () => {
+      color.setRed(54);
+      expect(color.getRed()).toBe(54);
+      expect(color.getGreen()).toBe(138);
+      expect(color.getBlue()).toBe(174);
+      expect(color.getHexValue()).toBe("#368AAE");
+    })
+
+    it("Should throw an exception if the sedRed method's parameter is null", () => {
+      expect(() => color.setRed(null)).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the sedRed method's parameter is less than 0", () => {
+      expect(() => color.setRed(-3)).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the sedRed method's parameter is more than 255", () => {
+      expect(() => color.setRed(261)).toThrow(TypeError);
+    })
+
+    // GREEN
+    it("Testing setGreen method", () => {
+      color.setGreen(255);
+      expect(color.getRed()).toBe(17);
+      expect(color.getGreen()).toBe(255);
+      expect(color.getBlue()).toBe(174);
+      expect(color.getHexValue()).toBe("#11FFAE");
+    })
+
+    it("Should throw an exception if the setGreen method's parameter is null", () => {
+      expect(() => color.setGreen(null)).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setGreen method's parameter is less than 0", () => {
+      expect(() => color.setGreen(-12)).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setGreen method's parameter is more than 255", () => {
+      expect(() => color.setGreen(994)).toThrow(TypeError);
+    })
+
+    // BLUE
+    it("Testing setBlue method", () => {
+      color.setBlue(150);
+      expect(color.getRed()).toBe(17);
+      expect(color.getGreen()).toBe(138);
+      expect(color.getBlue()).toBe(150);
+      expect(color.getHexValue()).toBe("#118A96");
+    })
+
+    it("Should throw an exception if the setBlue method's parameter is null", () => {
+      expect(() => color.setBlue(null)).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setBlue method's parameter is less than 0", () => {
+      expect(() => color.setBlue(-1)).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setBlue method's parameter is more than 255", () => {
+      expect(() => color.setBlue(333)).toThrow(TypeError);
+    })
+
+    // HEXADECIMAL
+    it("Testing setHexValue method", () => {
+      color.setHexValue("#A955B4");
+      expect(color.getRed()).toBe(169);
+      expect(color.getGreen()).toBe(85);
+      expect(color.getBlue()).toBe(180);
+      expect(color.getHexValue()).toBe("#A955B4");
+    })
+
+    it("Should throw an exception if the setHexValue method's parameter is null", () => {
+      expect(() => color.setHexValue(null)).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setHexValue method's parameter is not correct 1", () => {
+      expect(() => color.setHexValue("A955B4")).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setHexValue method's parameter is not correct 2", () => {
+      expect(() => color.setHexValue("#A955B")).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setHexValue method's parameter is not correct 3", () => {
+      expect(() => color.setHexValue("#A955B41")).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setHexValue method's parameter is not correct 4", () => {
+      expect(() => color.setHexValue("#A955G4")).toThrow(TypeError);
+    })
+
+    it("Should throw an exception if the setHexValue method's parameter is not correct 5", () => {
+      expect(() => color.setHexValue("#a955b4")).toThrow(TypeError);
     })
 })
--- src/Color.ts
+++ src/Color.ts
@@ -4,8 +4,18 @@ export class Color {
     private blue: number;
     private hexadecimal: string;
 
-    constructor(red?: number, green?: number, blue?: number, hexadecimal?: string) {
-        if (hexadecimal) {
+    public constructor(hexadecimal: string);
+    public constructor(red: number, green: number, blue: number);
+
+    public constructor(...colorData: any[]) {
+
+        if (colorData.length === 0) {
+            throw new TypeError("Color constructor mush have at least one argument");
+        }
+
+        if (colorData.length === 1) {
+            // Constructeur à un argument 
+            let hexadecimal = colorData[0].toString();
             if (/^#[0-9A-F]{6}$/.test(hexadecimal)) {
                 this.hexadecimal = hexadecimal;
                 this.red = parseInt(hexadecimal.substring(1, 3), 16);
@@ -14,16 +24,21 @@ export class Color {
             } else {
                 throw new TypeError("Hexadecimal value is not correct");
             }
-        } else if (red && green && blue) {
+        }
+
+        if (colorData.length === 3) {
+            // Constructeur à 3 arguments
+            let red = parseFloat(colorData[0]);
+            let green = parseFloat(colorData[1]);
+            let blue = parseFloat(colorData[2]);
             if ((red < 0 || red > 255) || (green < 0 || green > 255) || (blue < 0 || blue > 255)) {
                 throw new TypeError("RGB values must be between 0 and 255");
+            } else {
+                this.red = red;
+                this.green = green;
+                this.blue = blue;
+                this.hexadecimal = "#" + this.rgbToHex(red) + this.rgbToHex(green) + this.rgbToHex(blue);
             }
-            this.red = red;
-            this.green = green;
-            this.blue = blue;
-            this.hexadecimal = "#" + this.rgbToHex(red) + this.rgbToHex(green) + this.rgbToHex(blue);
-        } else {
-            throw new TypeError("Color constructor mush have at least one argument");
         }
     }
 
@@ -51,5 +66,51 @@ export class Color {
     rgbToHex(rgb: number): string {
         let hex = Number(rgb).toString(16).toUpperCase();
         return hex.length === 1 ? "0" + hex : hex;
+    }
+
+    setRed(red: number): void {
+        if (red !== null) {
+            if (red >= 0 && red <= 255) {
+                this.red = red;
+                this.hexadecimal = this.hexadecimal.substring(0, 1) + this.rgbToHex(red) + this.hexadecimal.substring(3, 7);
+            }
+            else throw new TypeError("RGB values must be between 0 and 255");
+        }
+        else throw new TypeError("This method's argument must not be null");
+    }
+
+    setGreen(green: number): void {
+        if (green !== null) {
+            if (green >= 0 && green <= 255) {
+                this.green = green;
+                this.hexadecimal = this.hexadecimal.substring(0, 3) + this.rgbToHex(green) + this.hexadecimal.substring(5, 7);
+            } 
+            else  throw new TypeError("RGB values must be between 0 and 255");
+        } 
+        else throw new TypeError("This method's argument must not be null");
+    }
+
+    setBlue(blue: number): void {
+        if (blue !== null) {
+            if (blue >= 0 && blue <= 255) {
+                this.blue = blue;
+                this.hexadecimal = this.hexadecimal.substring(0, 5) + this.rgbToHex(blue);
+            } 
+            else  throw new TypeError("RGB values must be between 0 and 255");
+        } 
+        else throw new TypeError("This method's argument must not be null");
+    }
+
+    setHexValue(hexadecimal: string): void {
+        if (hexadecimal !== null) {
+            if (/^#[0-9A-F]{6}$/.test(hexadecimal)) {
+                this.hexadecimal = hexadecimal;
+                this.red = parseInt(hexadecimal.substring(1, 3), 16);
+                this.green = parseInt(hexadecimal.substring(3, 5), 16);
+                this.blue = parseInt(hexadecimal.substring(5, 7), 16);
+            }
+            else throw new TypeError("Hexadecimal value is not correct");
+        }
+        else throw new TypeError("This method's argument must not be null");
     }
 }
